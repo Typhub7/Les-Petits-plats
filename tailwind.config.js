33ebc30d1b64ddb8b6bfef75b65baf53a55f5f02
Typho7 <caro.d@free.fr>
2023-11-13 18:51:06 +0100

    Index Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./css/*.css",
+  ],  
   theme: {
-    extend: {},
+    extend: {
+      width: { 
+      '1440': '1440px', 
+      },
+      height: {
+      '666' : '666px'
+      },
+      backgroundImage: {
+        'background-plate': "url('/assets/header_background.jpg')"
+      }
+
+    },
   },
   plugins: [],
 }
