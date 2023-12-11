1d817f32f7dde4d0300cc84db80dadfdbdde0980
Typho7 <caro.d@free.fr>
2023-12-11 15:46:51 +0100

    Refactoring GlobalSearch with FOR IF

--- scripts/search/globalsearch.js
+++ scripts/search/globalsearch.js
@@ -1,31 +1,55 @@
 import { recipes } from '../../data/recipes.js'
 
-export const globalFilterText = (globalInputText) => 
-    recipes.filter(recipe => 
-    (recipe.description.toLowerCase().includes(globalInputText.toLowerCase()))
-    )
+export const globalFilterText = (globalInputText) => {
+    const filteredRecipes = [] 
+    for (const recipe of recipes) {
+        if (recipe.description.toLowerCase().includes(globalInputText.toLowerCase())) {
+            filteredRecipes.push(recipe) 
+        }
+    }
+    return filteredRecipes 
+} 
 
-export const globalFilterAppliance = (globalInputText) => 
-    recipes.filter(recipe => 
-    recipe.appliance.toLowerCase().includes(globalInputText.toLowerCase())
-    )
+export const globalFilterAppliance = (globalInputText) => {
+    const filteredRecipes = [] 
+    for (const recipe of recipes) {
+        if (recipe.appliance.toLowerCase().includes(globalInputText.toLowerCase())) {
+            filteredRecipes.push(recipe) 
+        }
+    }
+    return filteredRecipes 
+} 
 
-export const globalFilterUstensils = (globalInputText) =>
-    recipes.filter((recipe) => 
-    recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(globalInputText.toLowerCase()))
-    )
+export const globalFilterUstensils = (globalInputText) => {
+    const filteredRecipes = [] 
+    for (const recipe of recipes) {
+        if (recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(globalInputText.toLowerCase()))) {
+            filteredRecipes.push(recipe) 
+        }
+    }
+    return filteredRecipes 
+} 
 
-export const globalFilterIngredients = (globalInputText) =>
-    recipes.filter((recipe) =>
-    recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(globalInputText.toLowerCase()))
-    );
+export const globalFilterIngredients = (globalInputText) => {
+    const filteredRecipes = [] 
+    for (const recipe of recipes) {
+        if (recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(globalInputText.toLowerCase()))) {
+            filteredRecipes.push(recipe) 
+        }
+    }
+    return filteredRecipes 
+} 
 
 export const globalFilterAll = (globalInputText) => {
-    const textFilteredRecipes = globalFilterText(globalInputText)
-    const applianceFilteredRecipes = globalFilterAppliance(globalInputText)
-    const ustensilsFilteredRecipes = globalFilterUstensils(globalInputText)
-    const ingredientsFilteredRecipes = globalFilterIngredients(globalInputText)
+    const textFilteredRecipes = globalFilterText(globalInputText) 
+    const applianceFilteredRecipes = globalFilterAppliance(globalInputText) 
+    const ustensilsFilteredRecipes = globalFilterUstensils(globalInputText) 
+    const ingredientsFilteredRecipes = globalFilterIngredients(globalInputText) 
     
-    const allFilteredRecipes = [...new Set(textFilteredRecipes.concat(applianceFilteredRecipes, ustensilsFilteredRecipes, ingredientsFilteredRecipes))]
-    return allFilteredRecipes
+    const allFilteredRecipes = [] 
+    for (const recipe of [...new Set([...textFilteredRecipes, ...applianceFilteredRecipes, ...ustensilsFilteredRecipes, ...ingredientsFilteredRecipes])]) {
+        allFilteredRecipes.push(recipe) 
     }
+
+    return allFilteredRecipes 
+} 
