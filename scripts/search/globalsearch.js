066d9b527ec9a098fb32980eacc25fb6cb680cc4
Typho7 <caro.d@free.fr>
2023-12-28 09:56:14 +0100

    last code optimisation

--- scripts/search/globalsearch.js
+++ scripts/search/globalsearch.js
@@ -1,80 +1,13 @@
 import { recipes } from '../../data/recipes.js'
 import { createRecipeCards } from '../template/recipetemplate.js'
 
-export const globalFilterText = (globalInputText) => {
-    const filteredRecipes = []
-    const recipesLength = recipes.length
-    for (let i = 0; i < recipesLength; i++) {
-        const recipe = recipes[i]
-
-        if (recipe.description.toLowerCase().includes(globalInputText.toLowerCase()) && !filteredRecipes.includes(recipe)) {
-            filteredRecipes.push(recipe)
-            createRecipeCards(filteredRecipes)         
-        }
-    }
-    return filteredRecipes
-}
-
-export const globalFilterAppliance = (globalInputText,alreadyFilteredRecipes) => {
-    const filteredRecipes = alreadyFilteredRecipes
-    const recipesLength = recipes.length
-    for (let i = 0; i < recipesLength; i++) {
-        const recipe = recipes[i]
-
-        if (recipe.appliance.toLowerCase().includes(globalInputText.toLowerCase()) && !filteredRecipes.includes(recipe)) {
-            filteredRecipes.push(recipe)
-            createRecipeCards(filteredRecipes)     
-        }
-    }
-    return filteredRecipes
-}
-
-export const globalFilterUstensils = (globalInputText,alreadyFilteredRecipes) => {
-    const filteredRecipes = alreadyFilteredRecipes
-    const recipesLength = recipes.length
-    for (let i = 0; i < recipesLength; i++) {
-        const recipe = recipes[i]
-
-        if (recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(globalInputText.toLowerCase())) && !filteredRecipes.includes(recipe)) {
-            filteredRecipes.push(recipe)
-            createRecipeCards(filteredRecipes)
-        }
-    }
-    return filteredRecipes
-}
-
-export const globalFilterIngredients = (globalInputText, alreadyFilteredRecipes) => {
-    const filteredRecipes = alreadyFilteredRecipes
-    const recipesLength = recipes.length
-    for (let i = 0; i < recipesLength; i++) {
-        const recipe = recipes[i]
-
-        if ( recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(globalInputText.toLowerCase())) && !filteredRecipes.includes(recipe)) {
-            filteredRecipes.push(recipe)
-            createRecipeCards(filteredRecipes)
-        }
-    }
-    return filteredRecipes
-}
-
-export const globalFilterAll = (globalInputText) => {
-    const textFilteredRecipes = globalFilterText(globalInputText)
-    const applianceFilteredRecipes = globalFilterAppliance(globalInputText,textFilteredRecipes)
-    const ustensilsFilteredRecipes = globalFilterUstensils(globalInputText,applianceFilteredRecipes)
-    const ingredientsFilteredRecipes = globalFilterIngredients(globalInputText,ustensilsFilteredRecipes)
-
-    const allFilteredRecipes = [...new Set(ingredientsFilteredRecipes)]
-    return allFilteredRecipes
-} 
-
-/*739 M ops/s ± 0.23% 96.83 % faster*/
 
 /** Applies all filters (text, appliance, ustensils, ingredients) to the recipes and update display for each new recipe
  *
  * @param {string} globalInputText - The input text for filtering recipes.
  * @returns {Array} - An array of recipes that match the combined filter criteria.
  */
-/*export const globalFilterAll = (globalInputText) => {
+export const globalFilterAll = (globalInputText) => {
     const filteredRecipes = []
     const recipesLength = recipes.length
 
@@ -93,4 +26,4 @@ export const globalFilterAll = (globalInputText) => {
     }
 
     return filteredRecipes
-}*/
+}
